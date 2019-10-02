117ec00d73bbfbda67cee1597e955bcec4e2f956
jflynn22 <jflynn22@jhu.edu>
2019-10-01 21:35:29 -0400

    Fix level 1 exploit

--- lvl1/level.py
+++ lvl1/level.py
@@ -1,6 +1,17 @@
 from flask import Flask, request, render_template, make_response, Markup
 import flask
 
+'''
+This is the fix
+'''
+def fix(input_string):
+    for letter in input_string:
+        if letter == '<':
+            input_string = input_string.replace(letter, '&lt')
+        elif letter == '>':
+            input_string = input_string.replace(letter, '&rt')
+    return input_string
+
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def render():
@@ -8,7 +19,7 @@ def render():
     if request.method == 'GET':
         arg = request.args.get('query', '')
         arg = Markup(arg.encode("utf-8"))
-        print(arg)
+        arg = fix(arg)
         r = make_response(render_template('webpage.html', arg = arg))
         r.headers.set("X-XSS-Protection", "0")
         return r
