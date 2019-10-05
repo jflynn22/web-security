2aa5beac177c5282e573127e060847afda1543de
jflynn22 <jflynn22@jhu.edu>
2019-10-05 16:05:31 -0400

    Patch level 5

--- level5/level.py
+++ level5/level.py
@@ -1,39 +1,40 @@
-#   def get(self):
-#     # Disable the reflected XSS filter for demonstration purposes
-#     self.response.headers.add_header("X-XSS-Protection", "0")
- 
-#     # Route the request to the appropriate template
-#     if "signup" in self.request.path:
-#       self.render_template('signup.html', 
-#         {'next': self.request.get('next')})
-#     elif "confirm" in self.request.path:
-#       self.render_template('confirm.html', 
-#         {'next': self.request.get('next', 'welcome')})
-#     else:
-#       self.render_template('welcome.html', {})
-     
-#     return
 from flask import Flask, request, render_template, make_response, Markup
 import flask
+
+current_page = 'welcome'
+
+def fix(arg):
+    print(arg)
+    if ("javascript" in arg or "java" in arg):
+        return 'confirm'
+    else:
+        return arg
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def render():
+    global current_page
     arg = ''
     if request.method == 'GET':
-        if "signup" in request.full_path:
+        if current_page == "signup":
             arg = request.args.get('next', '')
             arg = Markup(arg.encode("utf-8"))
+            arg = fix(arg)
             r = make_response(render_template('signup.html', next = arg))
             r.headers.set("X-XSS-Protection", "0")
-        elif "confirm" in request.full_path:
+            current_page = 'confirm'
+        elif current_page == 'confirm':
             arg = request.args.get('next', 'welcome')
             arg = Markup(arg.encode("utf-8"))
+            arg = fix(arg)
+            r = make_response(render_template('signup.html', next = arg))
             r = make_response(render_template('confirm.html', next = arg))
             r.headers.set("X-XSS-Protection", "0")
+            current_page = 'welcome'
         else:
             r = make_response(render_template('welcome.html'))
             r.headers.set("X-XSS-Protection", "0")
+            current_page = 'signup'
         return r
 
 
