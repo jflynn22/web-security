50ea80ec962198313438b3a93df3fd2a1bb86b90
jflynn22 <jflynn22@jhu.edu>
2019-10-05 18:18:17 -0400

    Working on csp level 2

---
+++ level2/level-csp.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template, make_response, Markup
+import flask
+
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def render():
+    arg = ''
+    if request.method == 'GET':
+        r = make_response(render_template('index-csp.html'))
+        r.headers.set("X-XSS-Protection", "0")
+        r.headers['Content-Security-Policy'] = "media-src 'none'"
+        return r
+
+if __name__=='__main__':
+    app.run(debug=True)
