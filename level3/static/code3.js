217c4b2c37ee4841cb153215efe81f56d0a838f9
jflynn22 <jflynn22@jhu.edu>
2019-10-08 16:03:41 -0400

    level 3 CSP works

--- level3/static/code3.js
+++ level3/static/code3.js
@@ -1,6 +1,5 @@
       function chooseTab(num) {
         // Dynamically load the appropriate image.
-        //This is the fix
         var html = "Image " + parseInt(num) + "<br>";
         html += "<img src='/static/images/cloud" + num + ".jpg' />";
         $('#tabContent').html(html);
@@ -33,7 +32,70 @@
       }, false);
 
       $(document).ready(() => {
-        $("#tab1").click(chooseTab('1'))
-        $("#tab2").click(chooseTab('2'))
-        $("#tab3").click(chooseTab('3'))
+        $("#tab1").click(function() {
+          // Dynamically load the appropriate image.
+          //This is the fix
+          var html = "Image " + parseInt('1') + "<br>";
+          html += "<img src='/static/images/cloud" + '1' + ".jpg' />";
+          $('#tabContent').html(html);
+   
+          window.location.hash = '1';
+   
+          // Select the current tab
+          var tabs = document.querySelectorAll('.tab');
+          for (var i = 0; i < tabs.length; i++) {
+            if (tabs[i].id == "tab" + parseInt('1')) {
+              tabs[i].className = "tab active";
+              } else {
+              tabs[i].className = "tab";
+            }
+          }
+   
+          // Tell parent we've changed the tab
+          top.postMessage(self.location.toString(), "*");
+        })
+        $("#tab2").click(function() {
+          // Dynamically load the appropriate image.
+          //This is the fix
+          var html = "Image " + parseInt('2') + "<br>";
+          html += "<img src='/static/images/cloud" + '2' + ".jpg' />";
+          $('#tabContent').html(html);
+   
+          window.location.hash = '2';
+   
+          // Select the current tab
+          var tabs = document.querySelectorAll('.tab');
+          for (var i = 0; i < tabs.length; i++) {
+            if (tabs[i].id == "tab" + parseInt('2')) {
+              tabs[i].className = "tab active";
+              } else {
+              tabs[i].className = "tab";
+            }
+          }
+   
+          // Tell parent we've changed the tab
+          top.postMessage(self.location.toString(), "*");
+        })
+        $("#tab3").click(function() {
+          // Dynamically load the appropriate image.
+          //This is the fix
+          var html = "Image " + parseInt('3') + "<br>";
+          html += "<img src='/static/images/cloud" + '3' + ".jpg' />";
+          $('#tabContent').html(html);
+   
+          window.location.hash = '3';
+   
+          // Select the current tab
+          var tabs = document.querySelectorAll('.tab');
+          for (var i = 0; i < tabs.length; i++) {
+            if (tabs[i].id == "tab" + parseInt('3')) {
+              tabs[i].className = "tab active";
+              } else {
+              tabs[i].className = "tab";
+            }
+          }
+   
+          // Tell parent we've changed the tab
+          top.postMessage(self.location.toString(), "*");
+        })
       })
